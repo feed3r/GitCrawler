2025c4572c339df3595504b550ce14dd2b0af206
Alessio Pascucci <feeder81@gmail.com>
2024-06-15 00:44:40 +0200

    Add some printing

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# main.py
-
 from .scraper import scrape_git_commands
 from .kafka_producer import send_to_kafka
 from .config import load_config
@@ -10,8 +8,23 @@ def main() -> None:
     broker = config['kafka']['broker']
     topic = config['kafka']['topic']
 
+    print("Starting to scrape Git commands...")
     commands = scrape_git_commands(base_url)
+    print(f"Scraping completed. Found {len(commands)} commands.")
+
+    for command in commands:
+        print("------")
+        print(f"Command Name: {command.command_name}")
+        print(f"Description: {command.description}")
+        print(f"Usage: {command.usage}")
+        print(f"Options: {command.options}")
+        print(f"Examples: {command.examples}")
+        print(f"Details: {command.details}")
+        print("------")
+
+    print("Sending data to Kafka broker...")
     send_to_kafka(commands, broker, topic)
+    print("Data successfully sent.")
 
 if __name__ == "__main__":
     main()
